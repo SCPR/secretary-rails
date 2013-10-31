b0203167bed5fb443e2204481f6a75dfe33afe36
Bryan Ricker <bricker88@gmail.com>
2013-10-30 17:08:13 -0700

    Specs

--- spec/lib/secretary/has_secretary_spec.rb
+++ spec/lib/secretary/has_secretary_spec.rb
@@ -1,59 +1,52 @@
 require 'spec_helper'
 
 describe Secretary::HasSecretary do
-  describe "has_secretary?" do
+  let(:user) { create :user }
+
+  let(:new_story) {
+    build :story,
+      :headline         => "Cool Story, Bro",
+      :body             => "Some cool text.",
+      :logged_user_id   => user.id
+  }
+
+  let(:other_story) {
+    create :story,
+      :headline         => "Cooler Story, Bro",
+      :body             => "Some cooler text.",
+      :logged_user_id   => user.id
+  }
+
+
+  describe "::has_secretary?" do
     it "returns false if no has_secretary declared" do
       User.has_secretary?.should eq false
     end
 
     it "returns true if @_has_secretary is true" do
-      User.instance_variable_set(:@_has_secretary, true)
-      User.has_secretary?.should eq true
+      Story.has_secretary?.should eq true
     end
   end
 
-
-  describe '#changes' do
-    it 'is the built-in changes reverse-merged with custom changes' do
-      story = create :story, headline: "Cool Story, Bro"
-      story.headline = "Updated Headline!"
-      story.changes.keys.should eq ["headline"]
-      story.custom_changes['assets'] = { a: 1, b: 2 }
-      story.changes.keys.should eq ["headline", "assets"]
-      story.custom_changes.keys.should eq ['assets']
-    end
-  end
-
-
-  describe 'custom changes' do
-    it 'gets cleared after saved' do
-      story = create :story
-      story.custom_changes["something"] = ["old something", "new something"]
-      story.save!
-      story.custom_changes.should eq Hash[]
-    end
-  end
 
   describe "::has_secretary" do
-    before :each do
-      user = User.create(name: "Bryan")
-      Secretary::Test::Story.any_instance.stub(:logged_user_id).and_return(user.id)
+    it "sets @_has_secretary to true" do
+      Story.has_secretary?.should eq true
     end
 
-    let(:new_story)   { build :story, headline: "Cool Story, Bro", body: "Some cool text.") }
-    let(:other_story) { create :story, headline: "Cooler Story, Bro", body: "Some cooler text.") }
-
-    it "sets @has_secretary to true" do
-      Secretary::Test::Story.instance_variable_get(:@_has_secretary).should eq true
-      Secretary::Test::Story.has_secretary?.should eq true
+    it 'adds the model to Secretary.versioned_models' do
+      Story # Load the class
+      Secretary.versioned_models.should include "Story"
     end
 
     it "adds the has_many association for versions" do
-      new_story.should have_many(:versions).dependent(:destroy).class_name("Secretary::Version")
+      new_story.should have_many(:versions)
+        .dependent(:destroy).class_name("Secretary::Version")
     end
 
     it "has logged_user_id" do
       new_story.should respond_to :logged_user_id
+      new_story.should respond_to :logged_user_id=
     end
 
     it "generates a version on create" do
@@ -63,7 +56,7 @@ describe Secretary::HasSecretary do
       new_story.versions.count.should eq 1
     end
 
-    it "generates a version when a record is saved on update" do
+    it "generates a version when a record is changed" do
       other_story.update_attributes(headline: "Some Cool Headline?!")
       Secretary::Version.count.should eq 2
       other_story.versions.size.should eq 2
@@ -76,17 +69,49 @@ describe Secretary::HasSecretary do
       other_story.versions.size.should eq 1
     end
 
-    it "sends to Version.generate on update" do
-      Secretary::Version.should_receive(:generate).twice.with(other_story)
-      other_story.update_attributes(headline: "Some Cool Headline?!")
-    end
-
     it "destroys all versions when the object is destroyed" do
       other_story.update_attributes!(headline: "Changed the headline")
       other_story.versions.size.should eq 2
       Secretary::Version.count.should eq 2
       other_story.destroy
       Secretary::Version.count.should eq 0
+    end
+  end
+
+
+  describe '#changes' do
+    it 'is the built-in changes reverse-merged with custom changes' do
+      story = create :story, headline: "Original Headline"
+      story.headline = "Updated Headline!"
+      story.custom_changes['assets'] = [[], { a: 1, b: 2 }]
+
+      story.changes.should eq Hash[{
+        'headline' => ['Original Headline', "Updated Headline!"],
+        'assets'   => [[], { 'a' => 1, 'b' => 2 }]
+      }]
+    end
+  end
+
+
+  describe '#changed?' do
+    it 'checks if custom changes are present as well' do
+      other_story.changed?.should eq false
+      other_story.custom_changes['assets'] = [[], { a: 1, b: 2 }]
+      other_story.changed?.should eq true
+    end
+  end
+
+
+  describe '#custom_changes' do
+    it 'is a hash into which you can put things' do
+      other_story.custom_changes['something'] = ['old', 'new']
+    end
+
+    it 'gets cleared after saved' do
+      other_story.custom_changes["something"] = ["old", "new"]
+      other_story.custom_changes.should eq Hash[{"something" => ["old", "new"]}]
+      other_story.save!
+      other_story.custom_changes.should eq Hash[]
     end
   end
 end
--- spec/lib/secretary/versioned_attributes_spec.rb
+++ spec/lib/secretary/versioned_attributes_spec.rb
@@ -1,4 +1,50 @@
 require 'spec_helper'
 
 describe Secretary::VersionedAttributes do
+  describe '::versioned_attributes' do
+    it 'uses the attributes set in the model' do
+      Animal.versioned_attributes.should eq ["name", "color"]
+    end
+
+    it 'uses the column names minus the global ignores if nothing is set' do
+      Location.versioned_attributes.should eq ["title", "address"]
+    end
+
+    it 'subtracts unversioned attributes if they are set' do
+      Person.versioned_attributes.should eq ["age"]
+    end
+  end
+
+
+  describe '#version_hash' do
+    let(:person) { create :person }
+
+    it 'is empty for non-dirty objects' do
+      person.version_hash.should eq Hash[]
+    end
+
+    it "return a hash of changes for just the attributes we want" do
+      person.age  = 120
+      person.name = "Freddie"
+
+      person.version_hash.should eq Hash[{
+        "age" => [100, 120]
+      }]
+    end
+  end
+
+
+  describe '#versioned_attribute?' do
+    let(:person) { create :person }
+
+    it 'is true if the attribute should be versioned' do
+      person.versioned_attribute?("age").should eq true
+      person.versioned_attribute?(:age).should eq true
+    end
+
+    it 'is false if the attribute should not be versioned' do
+      person.versioned_attribute?("name").should eq false
+      person.versioned_attribute?("id").should eq false
+    end
+  end
 end
