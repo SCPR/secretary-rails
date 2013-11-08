0effc09ee3c6bcf4482b70393ae4f98ebb21beb5
Bryan Ricker <bricker88@gmail.com>
2013-11-08 09:36:00 -0800

    Remove unused file

--- spec/lib/secretary/dirty_associations_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe Secretary::DirtyAssociations do
-  let(:other_story) {
-    create :story,
-      :headline         => "Cooler Story, Bro",
-      :body             => "Some cooler text."
-  }
-end
