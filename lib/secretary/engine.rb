610533b7997aff9d2151c26e9c717ecaece37ab6
Bryan Ricker <bricker88@gmail.com>
2014-03-19 17:49:42 -0700

    Fix autoload path

--- lib/secretary/engine.rb
+++ lib/secretary/engine.rb
@@ -3,7 +3,7 @@ module Secretary
     # This is necessary to support rails 3, which doesn't autoload
     # the concerns directory
     config.autoload_paths << File.expand_path(
-      "../../../../app/models/concerns", __FILE__)
+      "../../../app/models/concerns", __FILE__)
 
     config.to_prepare do
       Secretary.config.user_class.constantize.instance_eval do
