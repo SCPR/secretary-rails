f3cbd1fdcf7d01ebd8fe7177012e86a26b9eca98
Bryan Ricker <bricker88@gmail.com>
2014-03-19 17:27:08 -0700

    Add models/concerns to autoload for rails 3

--- lib/secretary/engine.rb
+++ lib/secretary/engine.rb
@@ -1,5 +1,10 @@
 module Secretary
   class Engine < ::Rails::Engine
+    # This is necessary to support rails 3, which doesn't autoload
+    # the concerns directory
+    config.autoload_paths << File.expand_path(
+      "../../../../app/models/concerns", __FILE__)
+
     config.after_initialize do
       Secretary.config.user_class.constantize.instance_eval do
         include Secretary::UserActivityAssociation
